59b37393e52f4f06a7469974cc7f2019ddba5946
itisoxy <97172592+itisoxy@users.noreply.github.com>
2025-09-03 12:46:37 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_ieTogBZWm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4smlnlfi7inmbvr4ojqc8eoprj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7t0mqrhvqdlp78ngpvdnlpvmf1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
